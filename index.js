2d4522235ece1e09aad375dfa861aee946fbe0a9
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-21 22:42:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
-
 function produceDrivingRange(range) {
+  return function withinDrivingRange(value1, value2) {
+    let first_value = parseInt(value1.substring(0, value1.length - 2))
+    let second_value = parseInt(value2.substring(0, value2.length - 2))
+    let final_value = range - Math.abs(first_value - second_value)
+    if (final_value >= 0) {
+      return `within range by ${final_value}`
+    }
+    else {
+      return `${Math.abs(final_value)} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(range) {
   return function withinDrivingRange(value1, value2) {
     let first_value = parseInt(value1.substring(0, value1.length - 2))
     let second_value = parseInt(value2.substring(0, value2.length - 2))
@@ -23,6 +35,3 @@ function produceDrivingRange(range) {
 //
 // produceDrivingRange() - Returns a function that then calculates whether a given trip is within range. For example, produceDrivingRange(10) returns a function that will return false if the trip is over 10 blocks distance and true if the distance is within range. So produceDrivingRange returns a function that we can then use to calculate if a trip is too large for a driver. We recommend referencing the test/indexTest.js for more details.
 // produceTipCalculator() - Returns a function that then calculates a tip. For example, produceTipCalculator(.10) returns a function that calculates ten percent tip on a fare. produceTipCalculator(.20) returns a function that calculates twenty percent tip on a fare.
-// createDriver is a function that returns a Driver class. The class has reference to a driverId that is incremented each time a new driver is created. The rest of the code base does not have access to driverId.
-//
-// The functions should make use of closures to achieve the specified goals.
