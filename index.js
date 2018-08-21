2c99c3e8d84be51999a18ea179ca88eb8edff94d
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-21 22:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function produceDrivingRange(range) {
       return `In range`
     }
     else {
-      return ` ${final_value}blocks out of range`
+      return `${final_value} blocks out of range`
     }
   }
 }
