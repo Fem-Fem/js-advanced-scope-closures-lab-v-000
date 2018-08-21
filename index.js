98c8d92f08d51022f2dea52c506dd8e69cdf1cce
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-21 22:38:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function produceDrivingRange(range) {
     let second_value = parseInt(value2.substring(0, value2.length - 2))
     let final_value = Math.abs(range - Math.abs(first_value - second_value))
     if (final_value >= 0) {
-      return `In range`
+      return final_value
     }
     else {
-      return `within range by ${final_value}`
+      return final_value
     }
   }
 }
